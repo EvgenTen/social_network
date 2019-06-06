fcc1fa3c90fb5b09106c1fa7d37900c73a09bcfe
evgen <izerus@gmail.com>
2019-06-06 16:34:46 +0300

    qw

---
+++ src/components/Content.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://static.interestingengineering.com/images/APRIL/sizes/black_hole_resize_md.jpg"
+          alt="hoho"
+        />
+      </div>
+      <div>content</div>
+    </div>
+  );
+};
+export default Content;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navi">
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Settings</div>
+      </nav>
+    </div>
+  );
+};
+export default Navbar;

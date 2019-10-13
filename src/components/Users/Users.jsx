e8e0b17fdbc8adc3d9b8a93a761e178ba7192896
Evgen big PC <izerus@gmail.com>
2019-10-13 14:14:52 +0300

    css

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,8 +4,9 @@ import c from "./Users.module.css"
 const Users = () => {
   return (
     <div className={c.usersInfo}>
-      Users will be here
-      <img src="https://icon-library.net/images/face-icon-png/face-icon-png-17.jpg" />
+		<div>
+ 	    	 <img src="https://icon-library.net/images/face-icon-png/face-icon-png-17.jpg" alt="face"/>
+		</div>
     </div>
   );
 };

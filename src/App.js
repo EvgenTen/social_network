4ab55fd7f90a50aad866a2a998ab3c518479df96
Evga <evgen@engineer.com>
2019-10-15 19:40:20 +0300

    users2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
 import { Route } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Users from "./components/Users/Users";
+import UsersContainer from "./components/Users/UsersContainer";
 
 //----------------DATA--------------------------------Comment
 
@@ -18,7 +17,7 @@ const App = () => {
       <div className="app-wrapper-content">
         <Route path="/dialogs" render={() => <DialogsContainer />} />
         <Route path="/profile" render={() => <Profile />} />
-        <Route path="/users" render={() => <Users />} />
+        <Route path="/users" render={() => <UsersContainer />} />
       </div>
     </div>
   );
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,84 @@
 import React from "react";
-import c from "./Users.module.css"
+import c from "./Users.module.css";
 
-const Users = () => {
+let Users = props => {
+  if (props.users.length === 0) {
+    props.setUsers([
+      {
+        id: 1,
+        photoUrl:
+          "https://icon-library.net/images/face-icon-png/face-icon-png-17.jpg",
+        followed: false,
+        fullName: "Dmitry",
+        status: "I`m Boss",
+        location: { city: "Moscow", country: "Russia" }
+      },
+      {
+        id: 2,
+        photoUrl:
+          "https://icon-library.net/images/face-icon-png/face-icon-png-17.jpg",
+        followed: true,
+        fullName: "Oleg",
+        status: "I`m lego",
+        location: { city: "Vladivostok", country: "Russia" }
+      },
+      {
+        id: 3,
+        photoUrl:
+          "https://icon-library.net/images/face-icon-png/face-icon-png-17.jpg",
+        followed: false,
+        fullName: "Kurva",
+        status: "I`m Kurva",
+        location: { city: "Porvan", country: "Romania" }
+      }
+    ]);
+  }
+
   return (
-    <div className={c.usersInfo}>
-		<div>
- 	    	 <img src="https://icon-library.net/images/face-icon-png/face-icon-png-17.jpg" alt="face"/>
-		</div>
+    <div>
+      {props.users.map(u => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <img src={u.photoUrl} className={c.userPhoto} />
+            </div>
+            <div>
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}
+                >
+                  UnFollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <div>{u.fullName}</div>
+            <div>{u.status}</div>
+          </span>
+          <span>
+            <div>{u.location.country}</div>
+            <div>{u.location.city}</div>
+          </span>
+        </div>
+      ))}
     </div>
+
+    // <div className={c.usersInfo}>
+    // <div>
+    //   	 <img src="https://icon-library.net/images/face-icon-png/face-icon-png-17.jpg" alt="face"/>
+    // </div>
+    // </div>
   );
 };
 

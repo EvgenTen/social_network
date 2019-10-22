6a513056207ab53d01e57a43d388e99704d8bf92
Evga <evgen@engineer.com>
2019-10-22 21:03:07 +0300

    did mount added

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,63 +3,68 @@ import c from "./Users.module.css";
 import * as axios from "axios";
 import boy from "../../assets/images/boy.png";
 
-let Users = props => {
-  if (props.users.length === 0) {
+class Users extends React.Component {
+
+  componentDidMount() {
     axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then(response => {
-        debugger;
-        props.setUsers(response.data.items);
-      });
+    .get("https://social-network.samuraijs.com/api/1.0/users")
+    .then(response => {
+      this.props.setUsers(response.data.items);
+    })
   }
 
-  return (
-    <div>
-      {props.users.map(u => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img src={u.photos.small == null ? boy : u.photos.small} className={c.userPhoto} />
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  UnFollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
-            <div>{u.name}</div>
-            <div>{u.status}</div>
-            <div>{u.followed}</div>
-          </span>
-          <span>
-            <div>{"u.location.country"}</div>
-            <div>{"u.location.city"}</div>
-          </span>
-        </div>
-      ))}
-    </div>
+  render() {
+    return (
+      <div>
+        {this.props.users.map(u => (
+          <div key={u.id}>
+            <span>
+              <div>
+                <img
+                  src={u.photos.small == null ? boy : u.photos.small}
+                  className={c.userPhoto}
+                />
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}
+                  >
+                    UnFollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
+            </span>
+            <span>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+              <div>{u.followed}</div>
+            </span>
+            <span>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
+            </span>
+          </div>
+        ))}
+      </div>
 
-    // <div className={c.usersInfo}>
-    // <div>
-    //   	 <img src="https://icon-library.net/images/face-icon-png/face-icon-png-17.jpg" alt="face"/>
-    // </div>
-    // </div>
-  );
-};
+      // <div className={c.usersInfo}>
+      // <div>
+      //   	 <img src="https://icon-library.net/images/face-icon-png/face-icon-png-17.jpg" alt="face"/>
+      // </div>
+      // </div>
+    );
+  }
+}
 
 export default Users;
